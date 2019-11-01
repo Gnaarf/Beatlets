ffe07fd09ad29b01a39feb864eb95921851a0268
Gnaarf <gnaarf@gmx.de>
2019-11-01 21:51:12 +0100

    Initial commit + Data prefab • Data prefab holds ColorData

---
+++ Assets/Scripts/ColorData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorData : MonoBehaviour
+{
+    public static ColorData Instance;
+
+    [Header("Player")]
+    [SerializeField] Color _playerColor = Color.white;
+    Color PlayerColor { get => _playerColor; }
+
+    [Header("Obstacles")]
+    [SerializeField] Color _wallColor = Color.white;
+    Color WallColor { get => _wallColor; }
+
+    [Header("Attacks")]
+    [SerializeField] Color _attackColor = Color.white;
+    Color AttackColor { get => _attackColor; }
+
+    [SerializeField] Color _monitorColor = Color.white;
+    Color MonitorColor { get => _monitorColor; }
+
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+}
