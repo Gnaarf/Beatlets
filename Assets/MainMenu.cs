fa9973a752a814d842b7d4a1db3bf6843238ea9a
Matthis Hagen <matthis.hagen@o2online.de>
2019-11-02 18:27:36 +0100

    Main Menu changes

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        print("ENtering the function");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        print("Quit");
+    }
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
