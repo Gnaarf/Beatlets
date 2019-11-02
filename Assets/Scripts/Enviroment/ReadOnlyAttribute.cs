d8a12f18bc331210f99de4a2cfbdd94fce8f00fe
Gnaarf <gnaarf@gmx.de>
2019-11-02 20:42:18 +0100

    fixed compile error in ReadOnlyAttribute

--- Assets/Scripts/Enviroment/ReadOnlyAttribute.cs
+++ Assets/Scripts/Enviroment/ReadOnlyAttribute.cs
@@ -6,6 +6,7 @@ public class ReadOnlyAttribute : PropertyAttribute
 
 }
 
+#if UNITY_EDITOR
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 public class ReadOnlyDrawer : PropertyDrawer
 {
@@ -24,3 +25,4 @@ public class ReadOnlyDrawer : PropertyDrawer
         GUI.enabled = true;
     }
 }
+#endif
