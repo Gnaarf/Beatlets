09f1140f200b454ba0a39b04cab1d2d385e1d3fc
Gnaarf <gnaarf@gmx.de>
2019-11-02 17:36:50 +0100

    Spawn Position + Player reset on dying • added SpawnPosition to Arena • added "Attack" tag to Meteors • Collision handling: reset to spawn Position

--- Assets/Scripts/Player/PlayerDying.cs
+++ Assets/Scripts/Player/PlayerDying.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerDying : MonoBehaviour
 {
+    PlayerStateBehaviour _playerStateBehaviourRef;
+
+    private void Start()
+    {
+        _playerStateBehaviourRef = GetComponent<PlayerStateBehaviour>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Attack")
         {
-            print("u ded");
+            _playerStateBehaviourRef.ResetToSpawnPosition();
         }
     }
 }
---
+++ Assets/Scripts/Player/PlayerStateBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        ResetToSpawnPosition();
+    }
+
+    public void ResetToSpawnPosition()
+    {
+        transform.position = FindObjectOfType<PlayerSpawnPositionFlag>().transform.position;
+    }
+}
