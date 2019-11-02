1937bb5ef4c707df71469129141e1248fa7bfaf2
Gnaarf <gnaarf@gmx.de>
2019-11-02 23:23:12 +0100

    implemented Attack-Callbacks, attached it to Player-Coin-Collision • added DummyAttackPattern for testing

---
+++ Assets/Scripts/Enviroment/Attack/DummyAttackPattern.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyAttackPattern : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
