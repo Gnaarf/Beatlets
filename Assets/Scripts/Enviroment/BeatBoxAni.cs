9e6ec1accc629a02e93a04456d42c8fbca38783b
Gnaarf <gnaarf@gmx.de>
2019-11-03 15:36:16 +0100

    did stuff

--- Assets/Scripts/Enviroment/BeatBoxAni.cs
+++ Assets/Scripts/Enviroment/BeatBoxAni.cs
@@ -2,23 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeatBoxAni : MonoBehaviour, IOnBeat
+public class BeatBoxAni : MonoBehaviour
 {
     [SerializeField,Range(-1,1)]
     float rspeed = .5F;
-    bool onBeat = true; 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        gameObject.transform.Rotate(new Vector3(0,0,1), Time.deltaTime*rspeed*360);
-        if(onBeat){
-            transform.localScale = Vector3.one *1.3f;
-        }else{
-            transform.localScale = Vector3.one;
-        }
-    }
-    
-    public void OnBeat(int c){
-       onBeat = !onBeat; 
+        gameObject.transform.Rotate(new Vector3(0,0,1), Time.fixedDeltaTime*rspeed*360);
     }
 }
