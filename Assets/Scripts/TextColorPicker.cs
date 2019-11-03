f6207e8f2a147ee9307d09e5ef37461c637b2eea
Matthis Hagen <matthis.hagen@o2online.de>
2019-11-03 16:46:20 +0100

    Add ColorPicker and access to colors via enums

---
+++ Assets/Scripts/TextColorPicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextColorPicker : MonoBehaviour
+{
+    [SerializeField]
+    EColor color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI textMesh = GetComponent<TextMeshProUGUI>();
+
+        if (textMesh != null)
+        {
+            textMesh.color = ColorDataSingleton.GetColor(color);
+        }
+        else
+        {
+            Debug.Log("Sprite " + this.gameObject.name + " does not have a Spriterenderer");
+        }
+    }
+}
