940eac52ec2f87cc6050132d0641357cfbe39ccb
Gnaarf <gnaarf@gmx.de>
2019-11-06 20:02:24 +0100

    BeatBox looks at player

--- Assets/Scripts/Enviroment/BeatBoxAni.cs
+++ Assets/Scripts/Enviroment/BeatBoxAni.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class BeatBoxAni : MonoBehaviour
 {
+    [SerializeField]
+    Transform _player;
+
     [SerializeField,Range(-1,1)]
     float rspeed = .5F;
 
     void FixedUpdate()
     {
-        gameObject.transform.Rotate(new Vector3(0,0,1), Time.fixedDeltaTime*rspeed*360);
+        if(_player)
+        {
+            transform.up = _player.transform.position - transform.position;
+        }
+        else
+        {
+            gameObject.transform.Rotate(new Vector3(0, 0, 1), Time.fixedDeltaTime * rspeed * 360);
+        }
     }
 }
