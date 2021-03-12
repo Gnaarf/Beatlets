c3137bdaf52bbb9c31ab60dbf6451eb6d8d72372
Jan-Cord <gnaarf@gmx.de>
2021-03-12 11:35:36 +0100

    added underscorePrefix to private variable names in LasorGenerator.cs

--- Assets/Scripts/Enviroment/Attack/LasorGenerator.cs
+++ Assets/Scripts/Enviroment/Attack/LasorGenerator.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 public class LasorGenerator : MonoBehaviour, IOnBeat, IMusicSpeedFactor
 {
-    [SerializeField] GameObject lasorPrefab= default;
-    [SerializeField] Transform beatBoxTransform= default;
-    [SerializeField] ClipController clipController = default;
+    [SerializeField] GameObject _lasorPrefab = default;
+    [SerializeField] Transform _beatBoxTransform = default;
+    [SerializeField] ClipController _clipController = default;
 
-    int count;
-    float musicSpeedFactor = 1;
-
-    void OnEnable(){
-        count = 6;
-        GetComponent<BeatListener>().wait0=true;
-        clipController.SetActive(true);
+    int _count;
+    float _musicSpeedFactor = 1;
+    
+    void OnEnable()
+    {
+        _count = 6;
+        GetComponent<BeatListener>().wait0 = true;
+        _clipController.SetActive(true);
     }
 
     public void OnBeat(int c, BeatInfo beatInfo)
     {
-        if ( gameObject.activeInHierarchy ) {
-            if ( --count <= 0 ) {
-                clipController.SetActive(false);
+        if (gameObject.activeInHierarchy)
+        {
+            if (--_count <= 0)
+            {
+                _clipController.SetActive(false);
                 gameObject.SetActive(false);
             }
-            var go = Instantiate(lasorPrefab).GetComponent<Lasor>();
-            go.musicSpeedFactor = musicSpeedFactor;
-            go.transform.position = beatBoxTransform.position;
-            go.transform.rotation = beatBoxTransform.rotation;
+            var go = Instantiate(_lasorPrefab).GetComponent<Lasor>();
+            go.musicSpeedFactor = _musicSpeedFactor;
+            go.transform.position = _beatBoxTransform.position;
+            go.transform.rotation = _beatBoxTransform.rotation;
         }
     }
     public void SetMusicSpeedFactor(float musicSpeedFactor)
     {
-        this.musicSpeedFactor = musicSpeedFactor;
+        this._musicSpeedFactor = musicSpeedFactor;
     }
 }
